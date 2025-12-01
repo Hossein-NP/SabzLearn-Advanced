e545a9271e2eed7e94439f30f77a6812fc1b5b71
Hossein Noruzpur <143217596+Hossein-NP@users.noreply.github.com>
2025-11-30 23:14:03 -0800

    logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-addEventListener
+// Accordion handler
+let acHeader = document.querySelector(".course-accordion__header")
+
+function openHandler() {
+  document.querySelector(".course-accordion").classList.toggle("course-accordion--open")
+}
+
+acHeader.addEventListener("click", openHandler())
